c1e74dd5284ed76c60d89babccec47f6ba3bfbd3
jj-lu <2380228977@qq.com>
2023-04-10 23:03:57 +0800

    lowdb完成储存

--- tallyBook/routes/index.js
+++ tallyBook/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
+const lowdb = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapt = new FileSync(__dirname + '/../data/db.json')
+const db = lowdb(adapt)
+const shortId = require('shortid')
+
 /* GET home page. */
 router.get('/tallyBook', function (req, res, next) {
   // res.render('index', { title: 'Express' });
@@ -17,8 +23,13 @@ router.get('/tallyBook/create', (req, res) => {
 })
 
 router.post('/tallyBook', (req, res) => {
+  let id = shortId.generate()
   console.log(req.body, 'body')
-  res.send('添加成功')
+  db.get('account')
+    .unshift({ id, ...req.body })
+    .write()
+  // res.send('添加成功')
+  res.render('success', { msg: '添加成功' })
 })
 
 module.exports = router
